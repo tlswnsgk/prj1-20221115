da98b6a419b387bf4bd1a7eccae8f2f176111764
tlswnsgk <110441845+tlswnsgk@users.noreply.github.com>
2022-11-15 15:21:20 +0900

    221115 수정 전

---
+++ src/main/java/com/study/service/member/MemberService.java
@@ -0,0 +1,59 @@
+package com.study.service.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.study.domain.member.MemberDto;
+import com.study.mapper.member.MemberMapper;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberMapper mapper;
+
+	public int insert(MemberDto member) {
+		
+		return mapper.insert(member);
+	}
+
+	public List<MemberDto> list() {
+		// TODO Auto-generated method stub
+		return mapper.selectAll();
+	}
+
+	public MemberDto getById(String id) {
+		// TODO Auto-generated method stub
+		return mapper.selectById(id);
+	}
+
+	public int modify(MemberDto member) {
+		int cnt = 0;
+		
+		try {
+			return mapper.update(member);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return cnt; 
+	}
+
+	public int remove(String id) {
+		return mapper.deleteById(id);
+	}
+
+	public MemberDto getByEmail(String email) {
+		// TODO Auto-generated method stub
+		return mapper.selectByEmail(email);
+	}
+
+}
+
+
+
+
+
+
